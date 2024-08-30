6262d96293ad7db56f46516d8bd7eb141ee669b6
Mattia Mendicino <mattia.mendicino@studenti.unimi.it>
2024-08-31 00:06:21 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("MIPSWEB");
+document.addEventListener('DOMContentLoaded', async () => {
+
+    await render('app');
+
+});
+
+async function render(templateName: string) {
+    const element = document.getElementById(templateName);
+    if (element) {
+        const template = await fetch(`templates/${templateName}.ejs`).then(res => res.text());
+        if (template) {
+            element.innerHTML = ejs.render(template);
+        } else {
+            console.error(`No template found: templates/${templateName}.ejs`);
+        }
+    } else {
+        console.error(`No element found: ${templateName}`);
+    }
+}
