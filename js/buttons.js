f7d5ebb1c2bbf774fd9709de36b308ac194eadcb
Mattia Mendicino <mattia.mendicino@studenti.unimi.it>
2024-08-31 19:31:01 +0200

    -

---
+++ js/buttons.js
@@ -0,0 +1,10 @@
+"use strict";
+window.settings = function () {
+    console.log("Settings");
+};
+window.newFile = function () {
+    console.log("New file");
+};
+window.importFile = function () {
+    console.log("Import file");
+};
