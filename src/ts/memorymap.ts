b3090fffc1fe361cbc8fe4789381ae14124ef392
giusgad <67549788+giusgad@users.noreply.github.com>
2025-04-30 16:50:42 +0200

    memorymap: basic intervals visualization

--- src/ts/memorymap.ts
+++ src/ts/memorymap.ts
@@ -6,3 +6,18 @@ export async function toggleMemoryMap() {
     memoryMapActive = !memoryMapActive;
     await renderApp();
 }
+
+(window as any).isAddressInInterval = function (
+    addr: number,
+    intervals: [],
+): boolean {
+    for (let interval of intervals as any) {
+        if (
+            addr >= interval.cells[0].address &&
+            addr <= interval.cells[interval.cells.length - 1].address
+        ) {
+            return true;
+        }
+    }
+    return false;
+};
