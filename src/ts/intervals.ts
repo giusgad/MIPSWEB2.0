125535b083260f80f6f3cc70f48e5c820c90471f
giusgad <67549788+giusgad@users.noreply.github.com>
2025-05-06 09:18:00 +0200

    merge intervals

--- src/ts/intervals.ts
+++ src/ts/intervals.ts
@@ -1,5 +1,26 @@
 import { getFromStorage } from "./utils.js";
 import { vm } from "./virtual-machine.js";
+import { Binary } from "./virtual-machine/Utils.js";
+
+interface cell {
+    address: number;
+    binary: Binary;
+    tags: { name: string; type: string }[];
+}
+
+interface interval {
+    cells: cell[];
+    formats: {
+        address: string;
+        value: string;
+        valueGranularity: string;
+    };
+}
+
+interface userInterval {
+    start: number;
+    end: number;
+}
 
 export function getMemoryIntervals() {
     const memory = vm.getMemory();
@@ -20,12 +41,96 @@ export function getMemoryIntervals() {
         }
     }
     intervals.push(extendInterval(currentInterval, intervals.length));
+    mergeIntervals(intervals, getUserIntervals());
 
     intervals.forEach((interval) => addCellTags(interval));
     return intervals;
 }
 
-function addCellTags(interval: { cells: any[] }) {
+function getUserIntervals(): userInterval[] {
+    return (
+        getFromStorage("local", "user Intervals") ?? [
+            {
+                start: 4194324,
+                end: 4194348,
+            },
+            {
+                start: 268500984,
+                end: 268500996,
+            },
+            {
+                start: 268460984,
+                end: 268460992,
+            },
+        ]
+    );
+}
+
+function mergeIntervals(
+    intervals: interval[],
+    userIntervals: userInterval[],
+): interval[] {
+    let newIntervals: interval[] = [];
+    const memory = vm.cpu.memory;
+    userIntervals.sort((a, b) => a.start - b.start);
+    for (const userInterval of userIntervals) {
+        for (let interval of intervals) {
+            const intervalStart = interval.cells[0].address;
+            const intervalEnd =
+                interval.cells[interval.cells.length - 1].address;
+            // if the intervals overlap then merge them
+            if (
+                intervalStart <= userInterval.end &&
+                userInterval.start <= intervalEnd
+            ) {
+                for (
+                    let addr = intervalStart - 4;
+                    addr >= userInterval.start;
+                    addr -= 4
+                ) {
+                    interval.cells.unshift({
+                        address: addr,
+                        binary: memory.loadWord(new Binary(addr)),
+                        tags: [],
+                    });
+                }
+                for (
+                    let addr = intervalEnd + 4;
+                    addr <= userInterval.end;
+                    addr += 4
+                ) {
+                    interval.cells.push({
+                        address: addr,
+                        binary: memory.loadWord(new Binary(addr)),
+                        tags: [],
+                    });
+                }
+            } else {
+                const cells = [];
+                for (
+                    let addr = userInterval.start;
+                    addr <= userInterval.end;
+                    addr += 4
+                ) {
+                    cells.push({
+                        address: addr,
+                        binary: memory.loadWord(new Binary(addr)),
+                        tags: [],
+                    });
+                }
+                newIntervals.push(
+                    extendInterval(
+                        cells,
+                        intervals.length + newIntervals.length,
+                    ),
+                );
+            }
+        }
+    }
+    return [...intervals, ...newIntervals];
+}
+
+function addCellTags(interval: interval) {
     for (const cell of interval.cells) {
         if (cell.address === vm.assembler.textSegmentStart.getValue()) {
             cell.tags.push({ name: ".text", type: "section" });
